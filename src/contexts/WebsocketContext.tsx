80205c728ec4bf756ede9621de91338448273a63
Napat Wareedee <omegyzr@gmail.com>
2023-10-27 18:26:40 +0700

    [UPDATE] add env variables

--- src/contexts/WebsocketContext.tsx
+++ src/contexts/WebsocketContext.tsx
@@ -1,7 +1,11 @@
 import { createContext } from "react";
 import { Socket, io } from "socket.io-client";
 
-export const socket = io('http://localhost:3000');
+export const socket = io(
+    import.meta.env.VITE_MODE === 'prod' ?
+        import.meta.env.VITE_SERVER_PROD_PATH :
+        import.meta.env.VITE_SERVER_DEV_PATH
+);
 export const WebsocketContext = createContext<Socket>(socket);
 
 export const WebsocketProvider = WebsocketContext.Provider;
